fce318eb2b7114c50b28ba996508a7c2f089228f
leapkh <leapkh@yahoo.com>
2017-03-21 10:35:44 +0700

    Add SettingsFragment

---
+++ app/src/main/java/kh/edu/rupp/fe/ruppmad/SettingsFragment.java
@@ -0,0 +1,14 @@
+package kh.edu.rupp.fe.ruppmad;
+
+import android.app.Fragment;
+
+/**
+ * RUPPMAD
+ * Created by leapkh on 21/3/17.
+ */
+
+public class SettingsFragment extends Fragment {
+
+
+
+}
